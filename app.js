3e4d5b9dae1071319c20b34f4a389105fe479bc8
Marcos Leguizamón <marcos.leguizamonlp@hotmail.com>
2018-05-02 17:10:04 -0300

    Arreglo de repositorio

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+// catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
