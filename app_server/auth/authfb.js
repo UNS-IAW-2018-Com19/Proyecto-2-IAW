7d5d8008a1948ccf94d74f0727c8f36221ecfa4d
Marcos Leguizamón <marcos.leguizamonlp@hotmail.com>
2018-05-06 16:32:26 -0300

    findorcreate implementado

--- app_server/auth/authfb.js
+++ app_server/auth/authfb.js
@@ -2,21 +2,39 @@
 const passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
 var User = require('../models/users');
-var findOrCreate = require('mongoose-findorcreate');
-//passport setup
 
 passport.use(new Strategy({
     clientID: '179829129338029',
     clientSecret: '05e2f6ba0b420e05c57b42614bf42858', 
     callbackURL: "https://e-sportstournament.herokuapp.com/auth/facebook/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
-        if (err) { return done(err); }
-        done(null, user);
-      });
-    }
-  ));
+  function(accessToken, refreshToken, profile, done) {
+    //check user table for anyone with a facebook ID of profile.id
+    User.findOne({
+        'facebookid': profile.id 
+    }, function(err, user) {
+        if (err) {
+            return done(err);
+        }
+        //No user was found... so create a new user with values from Facebook (all the profile. stuff)
+        if (!user) {
+            user = new User({
+                username: profile.displayName,
+                email: profile.emails[0].value,
+                facebookid: profile.id,
+                //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+                facebook: profile._json
+            });
+            user.save(function(err) {
+                if (err) console.log(err);
+                return done(err, user);
+            });
+        } else {
+            //found user. Return
+            return done(err, user);
+        }
+    });
+  }));
 
   passport.serializeUser(function(user, done) {
     done(null, user);
@@ -28,7 +46,5 @@ passport.use(new Strategy({
 
   module.exports = passport;
   
- 
-
 
 
--- app_server/models/users.js
+++ app_server/models/users.js
@@ -7,6 +7,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    facebookid:{
+        type: Number,
+        required: true
+    },
     email:{ 
         type: String,
         required: true
